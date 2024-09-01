997bab56f3dac7f634bdb421486f72521345cd78
EssiJunior <nessipjunior@gmail.com>
2024-09-01 23:24:21 +0100

    -> 3. Installed saas to improve css styling... and created Loader component for splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Loader from "./components/Loader/Loader"
 
 function App() {
 
   return (
     <>
       <h1 className='font-extrabold text-blue-600 bg-blue-50 text-center'>Tailwind test</h1>
+      <Loader />
     </>
   )
 }
