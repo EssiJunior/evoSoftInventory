a089d39e9684773479980655721b3284bd22042a
EssiJunior <nessipjunior@gmail.com>
2024-09-02 00:19:39 +0100

    -> 4. Created splash screen

---
+++ src/hoc/SplashScreen/SplashScreen.tsx
@@ -0,0 +1,63 @@
+// REACT IMPORTS
+import React, { Component } from 'react';
+
+// COMPONENTS
+import Loader from '../../components/Loader/Loader'
+
+// STYLE
+import './splash_screen.scss'
+
+// ASSETS
+import logo from "../../assets/logo_text.png"
+
+// INTERFACES
+
+interface WithSplashScreenProps {
+    [key: string]: any;
+}
+
+const SplashContent = () => {
+    return (
+        <div className='splash'>
+            <img src={logo} alt="logo" loading='lazy' />
+            <h2>Manage your tasks easily</h2>
+            <Loader />
+        </div>
+    )
+}
+
+export default function withSplashScreen(WrappedComponent: React.ComponentType<WithSplashScreenProps>) {
+    return class extends Component<WithSplashScreenProps, { loading: boolean }> {
+        constructor(props: WithSplashScreenProps) {
+            super(props);
+            this.state = {
+                loading: true,
+            };
+        }
+
+        async componentDidMount() {
+            try {
+                // Put here your await requests/ API requests
+                setTimeout(() => {
+                    this.setState({
+                        loading: false,
+                    });
+                }, 6500);
+            } catch (err) {
+                console.log(err);
+                this.setState({
+                    loading: false,
+                });
+            }
+        }
+
+        render() {
+            // while checking user session, show "loading" message
+            if (this.state.loading) return SplashContent();
+
+            // otherwise, show the desired route
+            return <WrappedComponent {...this.props} />;
+        }
+    };
+}
+
